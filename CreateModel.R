2850cd28b2ff29c468ed57587f513fbd50a1ade0
dwaynedreakford <dwayneaford@gmail.com>
2017-08-14 18:04:42 -0400

    Add loadNgTables and prediction functions
    Add loadNgTables and prediction functions.

--- CreateModel.R
+++ CreateModel.R
@@ -11,11 +11,17 @@ library(data.table)
 freqCutoff1 <- 5
 freqCutoffN <- 3
 
+# Highest order of the model (and of ngram table to create).
+maxN <- 3
+
 # Create and save to disk the ngram tables used for scoring
 # during prediction.
-# - maxN - Highest order of ngram table to create.
 #
-createNGramTables <- function(maxN = 3, sampleSize=1000, workDir = getPartitionDir("train")) {
+createNGramTables <- function(sampleSize=1000, mSource=c("blogs", "news"), partition) {
+    workDir <- getPartitionDir(partition)
+    setwd(workDir)
+    print(paste(c("Working directory:", workDir), collapse=" "))
+    
     print("Creating ngram tables...")
     print(paste(c("maxN:", maxN, "sampleSize:", sampleSize), collapse=" "))
     
@@ -25,7 +31,7 @@ createNGramTables <- function(maxN = 3, sampleSize=1000, workDir = getPartitionD
         
         # Create the DTM and n term vector.
         print(paste(c("Creating DTM... n=", nVal), collapse=""))
-        mdtm <- dtmFromMCorpus(sampleMediaCorpus(sampleSize=sampleSize, mediaSource = "blogs"), nVal)
+        mdtm <- dtmFromMCorpus(sampleMediaCorpus(sampleSize=sampleSize, mediaSource = mSource), nVal)
         tc <- col_sums(mdtm)
         print(paste(c("Total ", nVal, "-grams: ", length(tc)), collapse=""))
         rm(mdtm)
@@ -40,18 +46,25 @@ createNGramTables <- function(maxN = 3, sampleSize=1000, workDir = getPartitionD
         # Create and write the n-gram lookup table.
         # Columns: ngram, score
         ngScore <- numeric(0)
+        ngPrefix <- character(0)
+        ngNw <- character(0)
         if ( nVal == 1 ) {
             # unigram score
             ngScore <- tc/sum(tc)
         } else {
             # ngram (where n > 1) score
-            prfix <- sapply(strsplit(names(tc), " ", fixed=TRUE),
+            ngPrefix <- sapply(strsplit(names(tc), " ", fixed=TRUE),
                             function(x) paste0(x[1:nVal-1], collapse=" "))
-            prfixCt <- minus1tc[prfix]
+            ngNw <- sapply(strsplit(names(tc), " ", fixed=TRUE),
+                               function(x) x[nVal])
+            prfixCt <- minus1tc[ngPrefix]
             ngScore <- tc/prfixCt
         }
-        ngTbl <- data.table(ng = names(tc), score = ngScore)
-        ngTblFile <- paste0("ng_", nVal, "_tbl.rds")
+        if ( nVal ==1 )
+            ngTbl <- data.table(nextword=names(tc), score=ngScore)
+        else
+            ngTbl <- data.table(prefix=ngPrefix, nextword=ngNw, score=ngScore)
+        ngTblFile <- getNgTblFileNm(nVal)
         print(paste(c("Writing: ", ngTblFile), collapse=""))
         setwd(getPartitionDir("train"))
         saveRDS(ngTbl, file = ngTblFile)
@@ -64,6 +77,101 @@ createNGramTables <- function(maxN = 3, sampleSize=1000, workDir = getPartitionD
         gc()
     } # for (nVal in 1:maxN)
 }
+
+loadNgTables <- function(partition) {
+    setwd(getPartitionDir(partition))
+    ngTables <- list()
+    for ( nVal in 1:maxN ) {
+        ngTblFile <- getNgTblFileNm(nVal)
+        print(paste0("Loading ngram table from file: ", ngTblFile, collapse = ""))
+        ngTbl <- readRDS(ngTblFile)
+        ngTables[[nVal]] <- ngTbl
+    }
+    ngTables
+}
+
+getNgTblFileNm <- function(nVal) {
+    paste0("ng_", nVal, "_tbl.rds")
+}
+
+# Get the last `howMany` words from `inText`.
+# Returns a character vector of length 1 containing
+# the last three words, separated by " ".
+#
+lastNWords <- function(howMany, inText) {
+    if ( length(inText) != 1 ) stop("Argument `inText` must be a character vector of length 1.")
+    if ( howMany > maxN ) stop(paste0("Argument `howMany` cannot be greater than `maxN` (", maxN, ")", collapse=""))
+    
+    wordTokens <- getNGrams(inText, 1)
+    lastwords <- character(0)
+    if ( length(wordTokens) >= howMany ) {
+        begin <- length(wordTokens)-howMany+1
+        end <- length(wordTokens)
+        lastwords <- wordTokens[begin:end]
+    } else {
+        lastwords <- wordTokens
+    }
+    paste0(lastwords, collapse = " ")
+}
+
+# Predict the next word...
+#
+# - `nOrder` is the order (e.g., 3, 4) of ngram lookup table with 
+#    which to start the search for a matching `nOrder-1` word prefix. 
+#    For example: 
+#    + To predict the third word, the input must be two words, and 
+#      we start the search with the trigram lookup table, seeking
+#      rows with matching two-word prefix.
+#    + To predict the next word given no input, we simply return 
+#      the highest ranking unigram scores (from the unigram table).
+#
+# - `ngPrefix` should be a character vector of length 1 produced by
+#   `lastNWords`
+#
+sbAlpha <- 0.4
+nextWordScores <- function(nOrder, ngPrefix, ngTables, alphaPow=1) {
+    if ( length(ngTables) < nOrder ) 
+        stop(paste0(nOrder, " Ngram tables are needed to predict based on ", nOrder-1, " words."))
+
+    print(paste0("Seeking next-word scores for prefix: ", ngPrefix))
+    print(paste0("nOrder=", nOrder))
+    
+    # Get the lookup table.
+    ngTable <- ngTables[[nOrder]]
+    sbScores <- data.frame(nextword=character(0), score=numeric(0))
+    
+    # If nOrder is 1, just rank unigrams
+    if ( nOrder == 1 ) {
+        sbScores <- head(ngTable[order(-score)], 20)
+        if ( nrow(sbScores) > 0 ) {
+            sbScores[, "score"] <- sbScores$score*alphaPow
+        }
+    }
+    # Otherwise, seek a match among the prefixes. Continue
+    # the search in the `nOrder-1` ngram table if none are found.
+    else if ( nOrder > 1 ) {
+        sbScores <- ngTable[prefix==ngPrefix]
+        if ( nrow(sbScores) > 0 ) {
+            sbScores[, "score"] <- sbScores$score*alphaPow
+        }
+        else {
+            minus1gram <- lastNWords(nOrder-2, ngPrefix)
+            sba <- sbAlpha * alphaPow
+            sbScores <- nextWordScores(nOrder-1, minus1gram, ngTables, sba)
+        }
+    }
+    
+    head(sbScores[order(-score)], 20)
+}
+
+predictionFlow <- function(testText = "that is") {
+    # To predict based on the last n words, we need the n+1 ngram table.
+    # I.e., to predict the third word, we need two words of input
+    predInput <- lastNWords(2, testText)
+    sbScores <- nextWordScores(2+1, predInput, ngTables)
+    sbScores
+}
+
 
 
 
